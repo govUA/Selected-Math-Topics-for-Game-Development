ec54809a2ee914b986fefd4291286c2fa6e3b054
DuffKI <kotlyarihor@gmail.com>
2025-04-10 16:00:00 +0300

    Addition/subtraction

--- hw5/main.cpp
+++ hw5/main.cpp
@@ -32,6 +32,28 @@ public:
     float w() const {
         return _mm_cvtss_f32(_mm_shuffle_ps(data, data, _MM_SHUFFLE(3, 3, 3, 3)));
     }
+
+    vector4& add(const vector4& other) {
+        data = _mm_add_ps(data, other.data);
+        return *this;
+    }
+
+    vector4& add(float x, float y, float z) {
+        __m128 temp = _mm_set_ps(0.0f, z, y, x);
+        data = _mm_add_ps(data, temp);
+        return *this;
+    }
+
+    vector4& sub(const vector4& other) {
+        data = _mm_sub_ps(data, other.data);
+        return *this;
+    }
+
+    vector4& sub(float x, float y, float z) {
+        __m128 temp = _mm_set_ps(0.0f, z, y, x);
+        data = _mm_sub_ps(data, temp);
+        return *this;
+    }
 };
 
 bool areEqual(float a, float b, float epsilon = 1e-6f) {
@@ -48,43 +70,104 @@ void resetConsoleColor() {
 
 void testPassed() {
     setConsoleColor(10);
-    std::cout << "Passed.\n\n";
+    std::cout << "\tPassed.\n\n";
+    resetConsoleColor();
+}
+
+void allTestsPassed(const std::string& group) {
+    setConsoleColor(10);
+    std::cout << "\tAll " << group << "tests passed.\n\n";
     resetConsoleColor();
 }
 
 int main() {
-    std::cout << "Tests:\n\n";
+    std::cout << "TESTS:\n\n";
 
     {
-        std::cout << "1. 3-parameter constructor\n";
-        vector4 v(1.0f, 2.0f, 3.0f);
-        assert(areEqual(v.x(), 1.0f));
-        assert(areEqual(v.y(), 2.0f));
-        assert(areEqual(v.z(), 3.0f));
-        assert(areEqual(v.w(), 0.0f));
-        testPassed();
+        std::cout << "1. Struct base tests:\n\n";
+        {
+            std::cout << "\t1. 3-parameter constructor:\n";
+            vector4 v(1.0f, 2.0f, 3.0f);
+            assert(areEqual(v.x(), 1.0f));
+            assert(areEqual(v.y(), 2.0f));
+            assert(areEqual(v.z(), 3.0f));
+            assert(areEqual(v.w(), 0.0f));
+            testPassed();
+        }
+
+        {
+            std::cout << "\t2. 4-parameter constructor:\n";
+            vector4 v(4.0f, 5.0f, 6.0f, 7.0f);
+            assert(areEqual(v.x(), 4.0f));
+            assert(areEqual(v.y(), 5.0f));
+            assert(areEqual(v.z(), 6.0f));
+            assert(areEqual(v.w(), 7.0f));
+            testPassed();
+        }
+
+        {
+            std::cout << "\t3. Edge case values:\n";
+            vector4 v(-1.5f, 0.0f, 1e10f, -1e10f);
+            assert(areEqual(v.x(), -1.5f));
+            assert(areEqual(v.y(), 0.0f));
+            assert(areEqual(v.z(), 1e10f));
+            assert(areEqual(v.w(), -1e10f));
+            testPassed();
+        }
+
+        allTestsPassed("struct base ");
     }
 
     {
-        std::cout << "2. 4-parameter constructor\n";
-        vector4 v(4.0f, 5.0f, 6.0f, 7.0f);
-        assert(areEqual(v.x(), 4.0f));
-        assert(areEqual(v.y(), 5.0f));
-        assert(areEqual(v.z(), 6.0f));
-        assert(areEqual(v.w(), 7.0f));
-        testPassed();
+        std::cout << "2. Addition/subtraction tests:\n\n";
+        {
+            std::cout << "\t1. vector addition:\n";
+            vector4 a(1.0f, 2.0f, 3.0f);
+            vector4 b(4.0f, 5.0f, 6.0f);
+            a.add(b);
+            assert(areEqual(a.x(), 5.0f));
+            assert(areEqual(a.y(), 7.0f));
+            assert(areEqual(a.z(), 9.0f));
+            assert(areEqual(a.w(), 0.0f));
+            testPassed();
+        }
+
+        {
+            std::cout << "\t2. vector-value addition:\n";
+            vector4 v(1.0f, 2.0f, 3.0f);
+            v.add(10.0f, 20.0f, 30.0f);
+            assert(areEqual(v.x(), 11.0f));
+            assert(areEqual(v.y(), 22.0f));
+            assert(areEqual(v.z(), 33.0f));
+            assert(areEqual(v.w(), 0.0f));
+            testPassed();
+        }
+
+        {
+            std::cout << "\t3. vector subtraction:\n";
+            vector4 a(10.0f, 20.0f, 30.0f);
+            vector4 b(1.0f, 2.0f, 3.0f);
+            a.sub(b);
+            assert(areEqual(a.x(), 9.0f));
+            assert(areEqual(a.y(), 18.0f));
+            assert(areEqual(a.z(), 27.0f));
+            assert(areEqual(a.w(), 0.0f));
+            testPassed();
+        }
+
+        {
+            std::cout << "\t4.vector-value subtraction:\n";
+            vector4 v(5.0f, 10.0f, 15.0f);
+            v.sub(1.0f, 2.0f, 3.0f);
+            assert(areEqual(v.x(), 4.0f));
+            assert(areEqual(v.y(), 8.0f));
+            assert(areEqual(v.z(), 12.0f));
+            assert(areEqual(v.w(), 0.0f));
+            testPassed();
+        }
+        allTestsPassed("addition/subtraction");
     }
 
-    {
-        std::cout << "3. Edge case values\n";
-        vector4 v(-1.5f, 0.0f, 1e10f, -1e10f);
-        assert(areEqual(v.x(), -1.5f));
-        assert(areEqual(v.y(), 0.0f));
-        assert(areEqual(v.z(), 1e10f));
-        assert(areEqual(v.w(), -1e10f));
-        testPassed();
-    }
-
-    std::cout << "All tests passed.\n";
+    std::cout << "ALL TESTS PASSED.\n";
     return 0;
 }
